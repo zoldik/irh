deebb03dffd26803db3811d339cd86d7fad9dc02
francois.cojean <francois.cojean@44398326-d649-0410-948e-53f8f13d1be3>
2008-04-04 11:49:35 +0000

    Premiere ebauche d'un helloworld mvc avec spring

---
+++ src/web/HelloWorldController.java
@@ -0,0 +1,23 @@
+package web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(	HttpServletRequest arg0,
+										HttpServletResponse arg1) throws Exception
+	{
+		String aMessage = "Hello World MVC!";
+		 
+		ModelAndView myModelAndView = new ModelAndView("hello_world");
+		myModelAndView.addObject("message", aMessage);
+ 
+		return myModelAndView;
+	}
+
+}
