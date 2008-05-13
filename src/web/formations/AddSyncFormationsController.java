bf58bab5b9dfd333e98db488e93a3860fbc29476
francois.cojean <francois.cojean@44398326-d649-0410-948e-53f8f13d1be3>
2008-05-13 21:24:10 +0000

    - Ajout du jar jaxen-1.1-beta-7.jar. - Modification du controller AddSyncFormationsController  -> parsage du fichier xml (balise <organisme> et <formations_a_ajouter_modifier>)
    Prochaine etape :
    - On parse la balise <formations_a_supprimer>.
    - On gere les competences pour les formations.

--- src/web/formations/AddSyncFormationsController.java
+++ src/web/formations/AddSyncFormationsController.java
@@ -1,9 +1,13 @@
 package web.formations;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Node;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
@@ -11,15 +15,19 @@ import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.multipart.support.StringMultipartFileEditor;
 
 import entities.TextFileUploadBean;
+
 import entities.Formation;
+import entities.Organisme;
 
 import services.IServiceFormation;
+import services.IServiceOrganisme;
 
 
 
 public class AddSyncFormationsController extends SimpleFormController {
 	
 	private IServiceFormation sf;
+	private IServiceOrganisme so;
 
 	/* (non-Javadoc)
 	 * @see org.springframework.web.servlet.mvc.AbstractFormController#formBackingObject(javax.servlet.http.HttpServletRequest)
@@ -54,6 +62,7 @@ public class AddSyncFormationsController extends SimpleFormController {
 	/* (non-Javadoc)
 	 * @see org.springframework.web.servlet.mvc.SimpleFormController#onSubmit(java.lang.Object)
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	protected ModelAndView onSubmit(Object command) throws Exception {
 		TextFileUploadBean file = (TextFileUploadBean)command;
@@ -65,9 +74,103 @@ public class AddSyncFormationsController extends SimpleFormController {
         }
         else
         {
-        	// Gestion du fichier xml
-    		System.out.println("DEBUG : FICHIER XML");
-    		System.out.println(fichierXml);
+        	// TODO : Verifier la validité du fichier
+        	
+        	// Chargement du fichierXml dans le parseur xml
+            Document document = DocumentHelper.parseText(fichierXml);
+            
+            // Recuperation du nom de l'organisme
+            Node node = document.selectSingleNode( "//organisme/nom" );
+            String nomOrganisme = node.getText();
+            
+            // Recuperation de la description de l'organisme
+            node = document.selectSingleNode( "//organisme/description" );
+            String descriptionOrganisme = node.getText();
+            
+             
+            // Verification de la presence de l'organisme
+            boolean organismePresent = false;
+            int idOrganisme = -1;
+            List<Organisme> listeOrganismes = so.listOrganismes();
+            for (Organisme organisme : listeOrganismes) {
+				if(organisme.getNom().equals(nomOrganisme))
+				{
+					organismePresent = true;
+					idOrganisme = organisme.getId();
+				}
+			}
+            // Si il n'est pas present on l'ajoute.
+            if(!organismePresent)
+            {
+            	Organisme orga = new Organisme();
+            	orga.setNom(nomOrganisme);
+            	orga.setDescription(descriptionOrganisme);
+            	
+            	so.addOrganisme(orga);
+            }            
+            // Sinon, on le modifie
+            else
+            {
+            	Organisme orga = so.getOrganisme(idOrganisme);
+            	orga.setDescription(descriptionOrganisme);
+            	
+            	so.updateOrganisme(orga);
+            }
+            
+        	// On recupere la collection de formation a ajouter ou modifier
+            List<Node> nodes = document.selectNodes( "//organisme/formations_a_ajouter_modifier/formation" );
+    
+        	// Pour chaque noeud xml de formation,
+            for (Node n : nodes)
+            {
+            	// Recuperation des informations de la formation
+            	String libelleFormation = n.selectSingleNode("./libelle").getText();
+            	double prixParPersonneFormation = Double.parseDouble(n.selectSingleNode("./prix_par_personne").getText());
+            	double dureeFormation = Double.parseDouble(n.selectSingleNode("./duree").getText());
+            	// Verification la presence de la formation
+            	boolean formationPresente = false;
+            	int idFormation = -1;
+            	List<Formation> formations = sf.listFormations();
+            	for (Formation formation : formations) {
+					if(formation.getLibelle().equals(libelleFormation))
+					{
+						formationPresente = true;
+						idFormation = formation.getId();
+					}
+				}
+            	// Si, elle n'est pas presente, on l'ajoute.
+            	if(!formationPresente)
+            	{
+            		Formation f = new Formation();
+            		f.setLibelle(libelleFormation);
+            		f.setDuree(dureeFormation);
+            		f.setOrganisme(so.getOrganisme(idOrganisme));
+            		f.setPrixParPersonne(prixParPersonneFormation);
+            		
+            		sf.addFormation(f);
+            	}
+        		// Sinon, on la modifie
+            	else
+            	{
+            		Formation f = sf.getFormation(idFormation);
+            		f.setDuree(dureeFormation);
+            		f.setOrganisme(so.getOrganisme(idOrganisme));
+            		f.setPrixParPersonne(prixParPersonneFormation);
+            		
+            		sf.updateFormation(f);
+            	}
+			}
+        		
+        		
+        
+	         // On recupere la collection de formation a supprimer       
+	        	// Pour chaque formation
+	        		// Verification la presence de la formation
+	        		// Si, elle est presente, on la supprime.
+		   
+           //TODO : gerer les competences associees a chaque formation.
+            
+            
         }		
 		
 		return new ModelAndView(new RedirectView(this.getSuccessView()));
@@ -79,5 +182,13 @@ public class AddSyncFormationsController extends SimpleFormController {
 
 	public void setSf(IServiceFormation sf) {
 		this.sf = sf;
+	}
+
+	public IServiceOrganisme getSo() {
+		return so;
+	}
+
+	public void setSo(IServiceOrganisme so) {
+		this.so = so;
 	}
 }
